d75f5c24a57f187264111e49678091bb56b5039f
Gabriel H <gabrielhaberkamp@gmail.com>
2023-11-24 12:26:32 -0300

    Adição de estatísticas

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,4 +1,3 @@
-import Section from "../section";
 const Content=(props)=> {
 	return (
 		<div>
@@ -43,7 +42,83 @@ const Content=(props)=> {
 				<p>Buscando proporcionar avaliações completas e abrangentes a todas as empresas envolvidas, será montada uma equipe composta por pessoas com diferentes tipos de deficiência, buscando estudar a qualidade e usabilidade do site ou app de diferentes perspectivas. Se o projeto crescer, será feita uma parceria com alguma empresa do ramo de Inteligência Artificial, buscando treinar um modelo que possa avaliar todos os sites e apps da mesma forma que uma pessoa faria, de modo a atender a essa maior demanda.</p>
 			</Section>
 			<Section title="Conclusões parciais">
+				<p>Durante a realização deste projeto de pesquisa, foi criado um formulário de pesquisa com o objetivo de avaliar o conhecimento das pessoas da área da programação sobre o tema acessibilidade digital. Esta pesquisa incluiu respostas de diversos públicos distintos, como desenvolvedores, professores ou mesmo entusiastas da área de programação. Ela foi realizada com um total de 44 participantes. Abaixo, estão as principais perguntas que foram realizadas aos entrevistados, seguidas das estatísticas e conclusões obtidas.</p>
+				<h3>você se considera</h3>
+				<ul>
+					<li>25% já atuavam como desenvolvedor júnior;</li>
+					<li>22,7% já atuavam como desenvolvedor pleno;</li>
+					<li>34,1% já atuavam como desenvolvedor sênior;</li>
+					<li>11,4% ainda não atuavam como desenvolvedores, sendo entusiastas da área;</li>
+					<li>2,2% atuavam como professores de informática;</li>
+					<li>2,2% atuavam na área de negócios.</li>
+				</ul>
+				<p>Dessa forma, a pesquisa proporcionou a coleta de informações vindas de diversas perspectivas distintas, com subgrupos bastante variados na área de programação.</p>
 				<p>Com a posterior realização e implementação deste projeto de pesquisa, espera-se que, em geral, as empresas levem mais o tema da acessibilidade digital em consideração e, assim, tragam experiências de uso melhores a todos os seus clientes, independente da existência de qualquer tipo de deficiência. Além disso espera-se que, ao longo do tempo, a web se torne um local mais acessível e inclusivo, de modo que esse tema seja mais disseminado e as empresas valorizem todos os seus clientes.</p>
+				<h3>Você já ouviu falar sobre acessibilidade digital, acessibilidade na web ou outro termo semelhante?</h3<
+				<ul>
+					<li>93,2% disseram que já ouviram falar sobre o tema;</li>
+					<li>2,2% disseram que nunca ouviram falar;</li>
+					<li>4,6% disseram que nunca prestaram atenção.</li>
+				</ul>
+				<p>Isso mostra algo positivo, pois grande parte dos participantes pelo menos já ouviu falar sobre o tema.</p>
+				<h3>Você já realizou algum teste ou verificação para saber se seu site ou aplicativo mobile está acessível para pessoas com deficiência?</h3>
+				<ul>
+					<li>13,6% disseram que sim, e já realizaram testes com alguma ferramenta usada por pessoas com deficiência, como leitores de tela;</li>
+					<li>6,8% disseram que sim, e já pediram para uma ou mais pessoas com deficiência testarem o site ou app;</li>
+					<li>34,1% disseram que já realizaram testes com alguma ferramenta, como o WAVE ou o Google Lighthouse;</li>
+					<li>45,4% disseram que nunca realizaram nenhum teste de acessibilidade no site ou app.</li>
+				</ul>
+				<p>Isso mostra que, embora não seja tão elevado, o número de programadores que nunca realizaram testes de acessibilidade ainda é significativo e abrange quase a metade dos participantes.</p>
+				<h3>Se você for um desenvolvedor web front-end, você já ouviu falar na especificação ARIA (Accessible Rich Internet applications)?</h3>
+				<h4>Considerando apenas os desenvolvedores front-end que participaram da pesquisa (29 de 44)</h4>
+			<ul>	
+					<li>31,1% disseram que sim, e já a utilizam em seu site ou app;</li>
+					<li>34,5% disseram que já ouviram falar, mas ainda não a utilizam em seu site ou app;</li>
+					<li>34,5% disseram que não, nunca ouviram falar.</li>
+				</ul>
+				<p>Ainda que esta pergunta tenha tido respostas bastante variadas, chamou atenção que mais da metade dos participantes já ouviram falar da especificação ARIA, um total de 65,6%. Esse número foi acima do imaginado inicialmente no projeto. Isso é algo positivo, pois mostra que o tema da acessibilidade digital não é tão desconhecido entre os participantes.</p>
+				<p>A verificação desses resultados considerando todos os desenvolvedores que responderam à pesquisa não foi feita, por não ser relevante para a pergunta em questão.</p>
+				<h3>Você acredita que, no momento atual, possui o conhecimento necessário para garantir ou melhorar a acessibilidade do seu site ou app?</h3>
+				<h4>Considerando todos os desenvolvedores</h4>
+				<ul>
+					<li>15,9% disseram que sim, acreditam que já possuem o conhecimento, e já começaram a melhorar a acessibilidade do site ou app;</li>
+					<li>31,9% disseram que sim, possuem o conhecimento, mas ainda não iniciaram esforços nessa área;</li>
+					<li>52,2% disseram que não possuem o conhecimento necessário.</li>
+					</ul>
+					<p>Isso mostra que, infelizmente, muitos desenvolvedores, independente da área de atuação, não possuem nem mesmo o conhecimento necessário para melhorar a acessibilidade de seus sites ou apps, o que foi dito por mais da metade dos participantes.</p>
+					<h4>Considerando apenas os desenvolvedores front-end</h4>
+					<ul>
+						<li>24,1% disseram que sim, acreditam que já possuem o conhecimento, e já começaram a melhorar a acessibilidade do site ou app;</li>
+						<li>34,5% disseram que sim, possuem o conhecimento, mas ainda não iniciaram esforços nessa área;</li>
+						<li>41,4% disseram que não possuem o conhecimento necessário.</li>
+					</ul>
+					<p>Ainda que os índices tenham sido um pouco melhores, isso mostra que muitos desenvolvedores realmente não possuem conhecimento na área de acessibilidade digital.</p>
+					<h3>O que você acredita que seria necessário fazer no seu site ou app, ou na empresa em que você trabalha, para difundir mais o tema da acessibilidade digital?</h3>
+					<p>Do total de 44 participantes, 21 responderam (47,7%). As principais considerações foram:</p>
+					<ul>
+						<li>Deve haver pessoas capacitadas ou pessoas com deficiência para testar/validar a acessibilidade;</li>
+						<li>Demonstrar a necessidade e importância da inclusão, por exemplo colocar textos explicativos no caso de um jogo;</li>
+						<li>Conscientização pública, devendo ser replicada pelos times de comunicação da empresa, trazendo o tema para o censo comum;</li>
+						<li>Talvez os contratantes (empresas) não possuam interesse no tema;</li>
+						<li>Aprender a usar coisas mais simples, como texto alternativo em imagens e tags semânticas, pode em muitos casos tirar a necessidade de uso de ferramentas mais avançadas como ARIA;</li>
+						<li>Incentivar o aumento de outros indicadores da empresa (além do lucro);</li>
+						<li>Presenciar os esforços que uma pessoa com deficiência faz para acessar o site ou app;</li>Grandes empresas devem trazer o tema como requisito para vagas de emprego e em seus módulos e certificações;</li>
+						<li>Mais canais e blogs que ensinem sobre o uso do ARIA e outros recursos, material escaço em português e com fácil compreenção;</li>
+					</ul>
+					<h3>Se desejar, escreva aqui mais algum comentário sobre seu site ou app e o tema acessibilidade digital.</h3>
+					<p>Do total de 44 participantes, 19 responderam (43,2%). As principais considerações foram:</p>
+					<ul>
+						<li>O questionário deveria ser voltado a designers e não programadores, pois programadores apenas fazem o que é pedido, e designers que fazem sketchs e outros materiais;</li>
+						<li>Um dos participantes tinha um app de dislexia para pessoas com dificuldade de leitura e escrita;</li>
+						<li>Um dos participantes disse que a sua empresa fazia testes de acessibilidade com os 2 tipos de dautonismo (dificuldade ao distinguir vermelho e verde e azul e amarelo);</li>
+						<li>Um dos participantes recomendou uma playlist sobre o assunto: <a href="https://www.youtube.com/watch?v=4NGe4dzlukc&list=PLc38fcMFcV_vvWOhMDriBlVocTZ8mKQzR">Link da playlist</a></li>
+						<li>As empresas e equipes de desenvolvimento devem entender que todos têm direito ao acesso, e que limitar o acesso limita a renda;</li>
+						<li>A acessibilidade muitas vezes é proposta pelos times de UX/UI, mas não interessa ao cliente;</li>Muitas vezes os sites são complicados demais, e os desenvolvedores esperam que as ferramentas encontrem todos os erros de acessibilidade;</li>
+						<li>Um deles disse que na sua empresa, a acessibilidade é obrigada por lei (um banco público);</li>
+						<li>Infelizmente a acessibilidade deve ser sim movida pela ganância, dificilmente haverá empatia real de gestores, desenvolvedores ou vendedores;</li>Um dos participantes apresentou um trabalho sobre o tema numa feira da IFSul;</li>
+						<li>Um dos participantes realizou um curso de produtor multimídia no IFSC em Palhossa, que era bilíngue (Libras e Português), e no curso era amplamente discutido o tema, e quando ele entrou no mercado de trabalho a realidade foi outra. Ele deu uma dica de iniciar um movimento de enviar comentários ou e-mails às empresas solicitando isso;</li>Um dos participantes disse que mesmo não sendo da área do front-end, estuda sobre o tema e busca aplicar a acessibilidade sempre que possível.</li>
+					</ul>
+					<p>Como conclusão sobre a pesquisa realizada, acredito que os indicadores são bastante divididos em relação ao tema, embora alguns deles tenham sido maiores que o esperado. Algo que precisa de mais atenção é a discussão ampla desse tema dentro dos times de desenvolvimento, além de um maior conhecimento e conscientização dos desenvolvedores sobre o tema, como através de treinamentos ou workshops feitos pelas empresas.</p>
 			</Section>
 		</div>
 	);
